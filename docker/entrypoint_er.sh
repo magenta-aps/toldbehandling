47a231460c949bfb524c0e85feb1d36eaf9f9967
Lars Peter Thomsen <larsp@magenta-aps.dk>
2023-06-27 17:14:14 +0200

    [#55986] Servér ER-diagrammer lokalt

--- docker/entrypoint_er.sh
+++ docker/entrypoint_er.sh
@@ -14,7 +14,7 @@ if [[ "$GENERATE_DB_DOCUMENTATION" = true ]]; then
     sleep 1
   done
 
-  java -jar /usr/local/share/schemaspy.jar -dp /usr/local/share/postgresql.jar -t pgsql -db $POSTGRES_DB -host $POSTGRES_HOST -u $POSTGRES_USER -p $POSTGRES_PASSWORD -o /doc/er_html
+  java -jar /usr/local/share/schemaspy.jar -dp /usr/local/share/postgresql.jar -t pgsql -db $POSTGRES_DB -host $POSTGRES_HOST -u $POSTGRES_USER -p $POSTGRES_PASSWORD -o /doc
   exec "$@"
 
 fi
