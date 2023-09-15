a9da0af573e47c9c4ef86c17a912280796cb38c2
Nick Janssen <nj@magenta-aps.dk>
2023-09-15 13:21:34 +0200

    [#57266] added filter to tf10 list and using it in the admin view

--- rest/aktør/management/commands/create_dummy_aktører.py
+++ rest/aktør/management/commands/create_dummy_aktører.py
@@ -1,47 +1,61 @@
 from django.core.management.base import BaseCommand
 
 from aktør.models import Afsender, Modtager
+import random
+import string
+
+# Source: Wikipedia
+company_names = [
+    "Arctic Umiaq Line",
+    "Atuagkat Bookstore",
+    "Bank of Greenland",
+    "Brugseni",
+    "Diskoline",
+    "Great Greenland Furhouse",
+    "Greenland Airport Authority",
+    "Greenland Brewhouse",
+    "Kalaallit Nunaata Radioa",
+    "KNI A/S",
+    "Nukissiorfiit",
+    "Nunaoil",
+    "Nuuk TV",
+    "Nuup Bussii",
+    "Pilersuisoq",
+    "Pisiffik",
+    "Post Greenland",
+    "Royal Arctic Line",
+    "Royal Greenland",
+    "Sermitsiaq",
+    "TELE Greenland",
+]
+
+
+def random_char(y):
+    # Returns a sequence of 'y' random chars
+    word = "".join(random.choice(string.ascii_letters) for x in range(y))
+    return word.capitalize()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        Afsender.objects.create(
-            navn="TestFirma1",
-            adresse="Testvej 42",
-            postnummer=1234,
-            by="Testby",
-            postbox=None,
-            telefon="123456",
-            cvr="12345678",
-        )
-        Afsender.objects.create(
-            navn="TestFirma2",
-            adresse="Testvej 44",
-            postnummer=1234,
-            by="Testby",
-            postbox="15",
-            telefon="123456",
-            cvr="12345679",
-        )
-        Modtager.objects.create(
-            navn="TestFirma1",
-            adresse="Testvej 42",
-            postnummer=1234,
-            by="Testby",
-            postbox=None,
-            telefon="123456",
-            cvr="12345678",
-            kreditordning=False,
-            indførselstilladelse=1,
-        )
-        Modtager.objects.create(
-            navn="TestFirma2",
-            adresse="Testvej 44",
-            postnummer=1234,
-            by="Testby",
-            postbox="15",
-            telefon="123456",
-            cvr="12345679",
-            kreditordning=True,
-            indførselstilladelse=2,
-        )
+        for company_name in company_names:
+            if company_names.index(company_name) % 2 == 0:
+                Afsender.objects.create(
+                    navn=company_name,
+                    adresse=random_char(5) + "vej " + str(random.randint(0, 20)),
+                    postnummer=random.randint(1000, 9999),
+                    by=random_char(5) + "by",
+                    postbox=None,
+                    telefon=str(random.randint(100000, 999999)),
+                    cvr=str(random.randint(10000000, 99999999)),
+                )
+            else:
+                Modtager.objects.create(
+                    navn=company_name,
+                    adresse=random_char(5) + "vej " + str(random.randint(0, 20)),
+                    postnummer=random.randint(1000, 9999),
+                    by=random_char(5) + "by",
+                    postbox=None,
+                    telefon=str(random.randint(100000, 999999)),
+                    cvr=str(random.randint(10000000, 99999999)),
+                )
