c2710b60f8f87ce31ccbf9ee63a39691d8908f33
Lars Peter Thomsen <larsp@magenta.dk>
2023-11-17 14:47:59 +0100

    [#58600] Postgres-backend til admin og ui

--- ui/project/settings.py
+++ ui/project/settings.py
@@ -83,11 +83,13 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
     },
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
