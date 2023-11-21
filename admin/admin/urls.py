4a2341048b2e17651e94a3566fbd71698c7156ee
Carsten Agger <carstena@magenta.dk>
2023-11-21 15:05:18 +0100

    [#58623] Fix salt issues for test server

--- admin/admin/urls.py
+++ admin/admin/urls.py
@@ -12,7 +12,7 @@ from admin import views
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="tf10_list")),
     path("login", common_views.LoginView.as_view(), name="login"),
-    path("logout", common_views.LogoutView.as_view(url="/"), name="logout"),
+    path("logout", common_views.LogoutView.as_view(url="/admin/"), name="logout"),
     path("api/<path:path>", common_views.RestView.as_view(), name="rest"),
     path("index", views.IndexView.as_view(), name="index"),
     path(
