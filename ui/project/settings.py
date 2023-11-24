fcce7d23d2d1f97ef3956fec88faa5aef645a1f6
Lars Peter Thomsen <larsp@magenta-aps.dk>
2023-11-24 12:00:39 +0100

    [#58508] mitid logging

--- ui/project/settings.py
+++ ui/project/settings.py
@@ -83,6 +83,33 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "gunicorn": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["gunicorn"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["gunicorn"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+
 WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
