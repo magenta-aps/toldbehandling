fb300a9d37f3b6c6e94ab031670a2fc2b9e02647
Lars Peter Thomsen <larsp@magenta-aps.dk>
2024-01-03 10:38:08 +0100

    fix: lenient key get in login

--- told-common/told_common/rest_client.py
+++ told-common/told_common/rest_client.py
@@ -954,9 +954,9 @@ class RestClient:
             else:
                 raise
         if (
-            saml_data["firstname"] != user["first_name"]
-            or saml_data["lastname"] != user["last_name"]
-            or saml_data["email"] != user["email"]
+            saml_data.get("firstname") != user["first_name"]
+            or saml_data.get("lastname") != user["last_name"]
+            or saml_data.get("email") != user["email"]
             or saml_data.get("cvr") != user["indberetter_data"]["cvr"]
         ):
             user = client.patch(f"user/cpr/{int(cpr)}", mapped_data)
