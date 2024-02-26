59733daaeab1d555e8c07e6b7a62205fd4c1ce9f
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-02-26 15:11:54 +0100

    test(rest): [#59827] created API test for payments list-endpoint

--- rest/payment/tests.py
+++ rest/payment/tests.py
@@ -2,13 +2,11 @@ import random
 import uuid
 from datetime import date, datetime, timedelta, timezone
 from decimal import Decimal
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, call, patch
 
 from anmeldelse.models import PrivatAfgiftsanmeldelse, Varelinje
 from django.conf import settings
 from django.contrib.auth.models import Permission, User
-from django.contrib.contenttypes.models import ContentType
-from django.forms import model_to_dict
 from django.test import TestCase
 from django.urls import reverse
 from payment.api import generate_payment_item_from_varelinje
@@ -21,6 +19,7 @@ from payment.schemas import (
     ProviderOrderDetailsResponse,
     ProviderPaymentDetailsResponse,
     ProviderPaymentPayload,
+    ProviderPaymentResponse,
 )
 from payment.utils import convert_keys_to_camel_case
 from project.test_mixins import RestMixin
@@ -48,7 +47,24 @@ class PaymentTest(TestCase):
             Permission.objects.get(codename="view_privatafgiftsanmeldelse"),
         )
 
-        # Create a test declaration
+        # Create a test afgiftstabel
+        cls.afgiftstabel = Afgiftstabel.objects.create(
+            gyldig_fra=datetime.now(timezone.utc).isoformat()
+        )
+
+        # Create a test vareafgiftssats
+        cls.vareafgiftssats_personbiler = Vareafgiftssats.objects.create(
+            afgiftstabel=cls.afgiftstabel,
+            afgiftsgruppenummer=72,
+            vareart_da="PERSONBILER Afgiften test",
+            vareart_kl="PERSONBILER Afgiften test",
+            enhed=Vareafgiftssats.Enhed.SAMMENSAT,
+            minimumsbeløb=None,
+            afgiftssats=Decimal("137.00"),
+            kræver_indførselstilladelse=False,
+        )
+
+        # Create test declarations
         db_declaration_name = (
             random.choice(["Jens", "Peter", "Hans", "Søren", "Niels"])
             + " "
@@ -70,24 +86,12 @@ class PaymentTest(TestCase):
             oprettet_af=cls.user,
         )
 
-        # Create a test afgiftstabel
-        cls.afgiftstabel = Afgiftstabel.objects.create(
-            gyldig_fra=datetime.now(timezone.utc).isoformat()
+        db_declaration_2_name = (
+            random.choice(["Jens", "Peter", "Hans", "Søren", "Niels"])
+            + " "
+            + random.choice(["Jensen", "Petersen", "Hansen", "Sørensen", "Nielsen"])
         )
 
-        # Create a test vareafgiftssats
-        cls.vareafgiftssats_personbiler = Vareafgiftssats.objects.create(
-            afgiftstabel=cls.afgiftstabel,
-            afgiftsgruppenummer=72,
-            vareart_da="PERSONBILER Afgiften test",
-            vareart_kl="PERSONBILER Afgiften test",
-            enhed=Vareafgiftssats.Enhed.SAMMENSAT,
-            minimumsbeløb=None,
-            afgiftssats=Decimal("137.00"),
-            kræver_indførselstilladelse=False,
-        )
-
-        # Create declaraton items
         cls.declaration_items = [
             Varelinje.objects.create(
                 privatafgiftsanmeldelse=cls.declaration,
@@ -97,6 +101,40 @@ class PaymentTest(TestCase):
                 fakturabeløb=1337,
                 afgiftsbeløb=668,
             )
+        ]
+
+        cls.declaration_2 = PrivatAfgiftsanmeldelse.objects.create(
+            cpr=random.randint(1000000000, 9999999999),
+            navn=db_declaration_2_name,
+            adresse="Ligustervænget " + str(random.randint(1, 100)),
+            by="TestBy",
+            postnummer=4321,
+            telefon=str(random.randint(100000, 999999)),
+            bookingnummer=str(random.randint(100000, 999999)),
+            leverandørfaktura_nummer=str(random.randint(100000, 999999)),
+            indførselstilladelse=None,
+            indleveringsdato=date.today() + timedelta(days=random.randint(10, 30)),
+            status=random.choice(["ny", "afvist", "godkendt"]),
+            oprettet_af=cls.user,
+        )
+
+        cls.declaration_2_items = [
+            Varelinje.objects.create(
+                privatafgiftsanmeldelse=cls.declaration_2,
+                vareafgiftssats=cls.vareafgiftssats_personbiler,
+                mængde=1,
+                antal=1,
+                fakturabeløb=7331,
+                afgiftsbeløb=866,
+            ),
+            Varelinje.objects.create(
+                privatafgiftsanmeldelse=cls.declaration_2,
+                vareafgiftssats=cls.vareafgiftssats_personbiler,
+                mængde=1,
+                antal=1,
+                fakturabeløb=0,
+                afgiftsbeløb=0,
+            ),
         ]
 
 
@@ -226,7 +264,7 @@ class NetsPaymentProviderTests(TestCase):
         )
 
     def test_nets_charge(self):
-        # TDODO: make this test when the MR with the new 'charge' method is merged
+        # TODO: make this test when the MR with the new 'charge' method is merged
         pass
 
 
@@ -285,7 +323,6 @@ class PaymentAPITests(PaymentTest):
         )
 
         self.assertEqual(resp.status_code, 201)
-
         mock_get_provider_handler.assert_called_once_with(provider_name)
         mock_nets_provider.create.assert_called_once_with(
             ProviderPaymentPayload(
@@ -323,11 +360,109 @@ class PaymentAPITests(PaymentTest):
             f"{settings.HOST_DOMAIN}/payment/checkout/{self.declaration.id}",
         )
 
-    def test_list(self):
+    @patch("payment.api.get_provider_handler")
+    def test_list_nets(self, mock_get_provider_handler):
+        # Create test data
+        (
+            test_payment_1,
+            fake_provider_payment_1,
+        ) = self._create_test_payment_with_fake_provider_payment(
+            status="created",
+            amount=1337,
+            declaration=self.declaration,
+            provider_payment_id="1234",
+        )
+
+        (
+            test_payment_2,
+            fake_provider_payment_2,
+        ) = self._create_test_payment_with_fake_provider_payment(
+            status="created",
+            amount=7331,
+            declaration=self.declaration_2,
+            provider_payment_id="5678",
+        )
+
+        fake_provider_payments = {
+            fake_provider_payment_1.payment_id: fake_provider_payment_1,
+            fake_provider_payment_2.payment_id: fake_provider_payment_2,
+        }
+
+        # Configure mock(s)
+        mock_nets_provider = MagicMock(
+            host="http://localhost:8000",
+            initial_status="created",
+            read=MagicMock(
+                side_effect=lambda x: (
+                    fake_provider_payments[x] if x in fake_provider_payments else None
+                )
+            ),
+        )
+        mock_get_provider_handler.return_value = mock_nets_provider
+
+        # Invoke the API endpoint
+        resp = self.client.get(
+            reverse("api-1.0.0:payment_list"),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+        )
+
+        self.assertEqual(resp.status_code, 200)
+        mock_get_provider_handler.assert_called_once_with("nets")
+        mock_nets_provider.read.assert_has_calls(
+            [
+                call(test_payment_1.provider_payment_id),
+                call(test_payment_2.provider_payment_id),
+            ]
+        )
+
+    def test_get_nets(self):
         pass
 
-    def test_get(self):
+    def test_refresh_nets(self):
         pass
 
-    def test_refresh(self):
-        pass
+    def _create_test_payment_with_fake_provider_payment(
+        self,
+        status: str,
+        amount: int,
+        declaration: PrivatAfgiftsanmeldelse,
+        provider_payment_id: str,
+        currency: str = "DKK",
+    ) -> tuple[Payment, ProviderPaymentResponse]:
+        test_payment = Payment.objects.create(
+            status=status,
+            amount=amount,
+            currency=currency,
+            reference=declaration.id,
+            declaration=declaration,
+            provider_payment_id=provider_payment_id,
+        )
+
+        fake_provider_payment = ProviderPaymentResponse(
+            payment_id=provider_payment_id,
+            payment_details=ProviderPaymentDetailsResponse(
+                invoice_details={},
+                card_details={},
+            ),
+            order_details=ProviderOrderDetailsResponse(
+                amount=amount,
+                currency=currency,
+                reference=declaration.id,
+            ),
+            consumer=ProviderConsumerResponse(
+                shipping_address={},
+                company=ProviderCompanyResponse(
+                    contact_details=ContactDetails(
+                        phone_number={},
+                    )
+                ),
+                private_person=ContactDetails(
+                    phone_number={},
+                ),
+                billing_address={},
+            ),
+            checkout={},
+            created=datetime.now(timezone.utc).isoformat(),
+        )
+
+        return test_payment, fake_provider_payment
