479aa23a26346287984c96feea6e48050a3121cd
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-02-26 10:32:11 +0100

    test: [#59827] created API test for payment-create using the NETs payment provider

--- rest/payment/tests.py
+++ rest/payment/tests.py
@@ -1,21 +1,106 @@
 import random
 import uuid
-from datetime import date, timedelta
-from unittest import mock
-from unittest.mock import patch
+from datetime import date, datetime, timedelta, timezone
+from decimal import Decimal
+from unittest.mock import MagicMock, patch
 
-from anmeldelse.models import PrivatAfgiftsanmeldelse
+from anmeldelse.models import PrivatAfgiftsanmeldelse, Varelinje
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission, User
+from django.contrib.contenttypes.models import ContentType
+from django.forms import model_to_dict
 from django.test import TestCase
-from payment.api import provider_payment_validation
+from django.urls import reverse
+from payment.api import generate_payment_item_from_varelinje
 from payment.models import Payment
 from payment.provider_handlers import get_provider_handler
-from payment.schemas import ProviderPaymentPayload
+from payment.schemas import (
+    ContactDetails,
+    ProviderCompanyResponse,
+    ProviderConsumerResponse,
+    ProviderOrderDetailsResponse,
+    ProviderPaymentDetailsResponse,
+    ProviderPaymentPayload,
+)
 from payment.utils import convert_keys_to_camel_case
+from project.test_mixins import RestMixin
+from project.util import json_dump
+from sats.models import Afgiftstabel, Vareafgiftssats
 
 
-class PaymentProviderTest(TestCase):
+class PaymentTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a test user which can handle payments
+        cls.user_permissions = [
+            Permission.objects.get(codename="view_payment"),
+            Permission.objects.get(codename="add_payment"),
+            Permission.objects.get(codename="change_payment"),
+            Permission.objects.get(codename="delete_payment"),
+        ]
+
+        cls.user, cls.user_token, cls.user_refresh_token = RestMixin.make_user(
+            username="payment-test-user",
+            plaintext_password="testpassword1337",
+            permissions=cls.user_permissions,
+        )
+        cls.user.user_permissions.add(
+            Permission.objects.get(codename="view_privatafgiftsanmeldelse"),
+        )
+
+        # Create a test declaration
+        db_declaration_name = (
+            random.choice(["Jens", "Peter", "Hans", "Søren", "Niels"])
+            + " "
+            + random.choice(["Jensen", "Petersen", "Hansen", "Sørensen", "Nielsen"])
+        )
+
+        cls.declaration = PrivatAfgiftsanmeldelse.objects.create(
+            cpr=random.randint(1000000000, 9999999999),
+            navn=db_declaration_name,
+            adresse="Ligustervænget " + str(random.randint(1, 100)),
+            by="TestBy",
+            postnummer=1234,
+            telefon=str(random.randint(100000, 999999)),
+            bookingnummer=str(random.randint(100000, 999999)),
+            leverandørfaktura_nummer=str(random.randint(100000, 999999)),
+            indførselstilladelse=None,
+            indleveringsdato=date.today() + timedelta(days=random.randint(10, 30)),
+            status=random.choice(["ny", "afvist", "godkendt"]),
+            oprettet_af=cls.user,
+        )
+
+        # Create a test afgiftstabel
+        cls.afgiftstabel = Afgiftstabel.objects.create(
+            gyldig_fra=datetime.now(timezone.utc).isoformat()
+        )
+
+        # Create a test vareafgiftssats
+        cls.vareafgiftssats_personbiler = Vareafgiftssats.objects.create(
+            afgiftstabel=cls.afgiftstabel,
+            afgiftsgruppenummer=72,
+            vareart_da="PERSONBILER Afgiften test",
+            vareart_kl="PERSONBILER Afgiften test",
+            enhed=Vareafgiftssats.Enhed.SAMMENSAT,
+            minimumsbeløb=None,
+            afgiftssats=Decimal("137.00"),
+            kræver_indførselstilladelse=False,
+        )
+
+        # Create declaraton items
+        cls.declaration_items = [
+            Varelinje.objects.create(
+                privatafgiftsanmeldelse=cls.declaration,
+                vareafgiftssats=cls.vareafgiftssats_personbiler,
+                mængde=1,
+                antal=1,
+                fakturabeløb=1337,
+                afgiftsbeløb=668,
+            )
+        ]
+
+
+class NetsPaymentProviderTests(TestCase):
     def setUp(self):
         self.handler = get_provider_handler(settings.PAYMENT_PROVIDER_NETS)
 
@@ -141,4 +226,108 @@ class PaymentProviderTest(TestCase):
         )
 
     def test_nets_charge(self):
+        # TDODO: make this test when the MR with the new 'charge' method is merged
+        pass
+
+
+class PaymentAPITests(PaymentTest):
+    @patch("payment.api.get_provider_handler")
+    def test_create_nets(self, mock_get_provider_handler):
+        provider_name = "nets"
+        fake_nets_payment = {
+            "paymentId": "1234",
+            "paymentDetails": ProviderPaymentDetailsResponse(
+                invoice_details={},
+                card_details={},
+            ),
+            "orderDetails": ProviderOrderDetailsResponse(
+                amount=1337,
+                currency="DKK",
+                reference="1234",
+            ),
+            "consumer": ProviderConsumerResponse(
+                shippingAddress={},
+                company=ProviderCompanyResponse(
+                    contact_details=ContactDetails(
+                        phone_number={},
+                    )
+                ),
+                privatePerson=ContactDetails(
+                    phone_number={},
+                ),
+                billingAddress={},
+            ),
+            "checkout": {},
+            "created": datetime.now(timezone.utc).isoformat(),
+        }
+
+        mock_nets_provider = MagicMock(
+            initial_status="created",
+            host=settings.PAYMENT_PROVIDER_NETS_HOST,
+            terms_url=settings.PAYMENT_PROVIDER_NETS_TERMS_URL,
+            create=MagicMock(return_value=fake_nets_payment),
+            read=MagicMock(return_value=fake_nets_payment),
+        )
+
+        mock_get_provider_handler.return_value = mock_nets_provider
+
+        # Invoke the API endpoint
+        resp = self.client.post(
+            reverse("api-1.0.0:payment_create"),
+            data=json_dump(
+                {
+                    "declaration_id": self.declaration.id,
+                    "provider": provider_name,
+                }
+            ),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+            content_type="application/json",
+        )
+
+        self.assertEqual(resp.status_code, 201)
+
+        mock_get_provider_handler.assert_called_once_with(provider_name)
+        mock_nets_provider.create.assert_called_once_with(
+            ProviderPaymentPayload(
+                declaration_id=self.declaration.id,
+                amount=38700,  # FYI: (137*100) + (250*100)
+                currency="DKK",
+                reference=self.declaration.id,
+                provider=provider_name,
+                items=[
+                    generate_payment_item_from_varelinje(self.declaration_items[0]),
+                    {
+                        "reference": "tillægsafgift",
+                        "name": "Tillægsafgift",
+                        "quantity": 1.0,
+                        "unit": "ant",
+                        "unit_price": 0,
+                        "tax_rate": 0,
+                        "tax_amount": 0,
+                        "gross_total_amount": 0,
+                        "net_total_amount": 0,
+                    },
+                    {
+                        "reference": "ekspeditionsgebyr",
+                        "name": "Ekspeditionsgebyr",
+                        "quantity": 1.0,
+                        "unit": "ant",
+                        "unit_price": 25000,
+                        "tax_rate": 0,
+                        "tax_amount": 0,
+                        "gross_total_amount": 25000,
+                        "net_total_amount": 25000,
+                    },
+                ],
+            ),
+            f"{settings.HOST_DOMAIN}/payment/checkout/{self.declaration.id}",
+        )
+
+    def test_list(self):
+        pass
+
+    def test_get(self):
+        pass
+
+    def test_refresh(self):
         pass
