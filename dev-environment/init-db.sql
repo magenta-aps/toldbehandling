dbad84bd341f3315c0e84ae9a7020421a0e1c32e
Peter Juelsgaard <pj@magenta.dk>
2024-03-04 15:49:10 +0100

    fix: [#59976] Bumped dev-postgres to 16, and completed psql-client installation

--- dev-environment/init-db.sql
+++ dev-environment/init-db.sql
@@ -1,7 +1,11 @@
 CREATE USER "toldbehandling-ui" WITH PASSWORD 'toldbehandling' CREATEDB;
 CREATE DATABASE "toldbehandling-ui";
 GRANT ALL PRIVILEGES ON DATABASE "toldbehandling-ui" TO "toldbehandling-ui";
+ALTER DATABASE "toldbehandling-ui" OWNER TO "toldbehandling-ui";
+GRANT ALL ON SCHEMA "public" TO "toldbehandling-ui";
 
 CREATE USER "toldbehandling-admin" WITH PASSWORD 'toldbehandling' CREATEDB;
 CREATE DATABASE "toldbehandling-admin";
 GRANT ALL PRIVILEGES ON DATABASE "toldbehandling-admin" TO "toldbehandling-admin";
+ALTER DATABASE "toldbehandling-admin" OWNER TO "toldbehandling-admin";
+GRANT ALL ON SCHEMA "public" TO "toldbehandling-admin";
