0d9c38750d0d96520d182cd8bb64de5210e2925e
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-03-08 12:29:00 +0100

    test(rest): [#60031] new coverate-test test_create_postforsendelse_bad_request

--- rest/forsendelse/tests.py
+++ rest/forsendelse/tests.py
@@ -2,9 +2,16 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from aktør.models import Afsender, Modtager
+from anmeldelse.models import Afgiftsanmeldelse, afgiftsanmeldelse_upload_to
+from common.models import IndberetterProfile
+from django.contrib.auth.models import Permission, User
+from django.forms import model_to_dict
 from django.test import TestCase
+from django.urls import reverse
 from forsendelse.models import Fragtforsendelse, Postforsendelse
-from project.test_mixins import RestTestMixin
+from project.test_mixins import RestMixin, RestTestMixin
+from project.util import json_dump
 
 
 class PostforsendelseTest(RestTestMixin, TestCase):
@@ -121,3 +128,140 @@ class FragtforsendelseTest(RestTestMixin, TestCase):
         self.assertIn(self.fragtforsendelse_data["fragtbrevsnummer"], string)
         self.assertIn(str(self.fragtforsendelse_data["forsendelsestype"].label), string)
         self.assertIn(self.fragtforsendelse_data["forbindelsesnr"], string)
+
+
+class PostforsendelseAPITests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_permissions = [
+            Permission.objects.get(codename="view_postforsendelse"),
+            Permission.objects.get(codename="add_postforsendelse"),
+        ]
+
+        cls.user, cls.user_token, cls.user_refresh_token = RestMixin.make_user(
+            username="payment-test-user",
+            plaintext_password="testpassword1337",
+            permissions=cls.user_permissions,
+        )
+
+    def test_create_postforsendelse_bad_request(self):
+        resp = self.client.post(
+            reverse("api-1.0.0:postforsendelse_create"),
+            data=json_dump(
+                {
+                    "payload": {
+                        "forsendelsestype": None,
+                        "postforsendelsesnummer": None,
+                        "afsenderbykode": None,
+                        "afgangsdato": None,
+                        "kladde": None,
+                    }
+                }
+            ),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+            content_type="application/json",
+        )
+
+        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(
+            resp.json(),
+            {
+                "__all__": [
+                    "Begrænsning “aktuel_har_postforsendelsesnummer” er overtrådt.",
+                    "Begrænsning “aktuel_har_afsenderbykode” er overtrådt.",
+                ]
+            },
+        )
+
+    def test_list_postforsendelser_filter_user_query_none(self):
+        # Create some test data to list
+        postforsendelse = Postforsendelse.objects.create(
+            forsendelsestype=Postforsendelse.Forsendelsestype.SKIB,
+            postforsendelsesnummer="1234567890",
+            afsenderbykode="1234",
+            afgangsdato="2023-01-01",
+            kladde=False,
+            oprettet_af=self.user,
+        )
+
+        resp = self.client.get(
+            reverse("api-1.0.0:postforsendelse_list"),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+        )
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json(), {"count": 0, "items": []})
+
+    def test_list_postforsendelser_filter_user_created_by_indberetter_cvr(self):
+        _ = IndberetterProfile.objects.create(
+            user=self.user,
+            cvr="13371337",
+        )
+
+        postforsendelse = Postforsendelse.objects.create(
+            forsendelsestype=Postforsendelse.Forsendelsestype.SKIB,
+            postforsendelsesnummer="1234567890",
+            afsenderbykode="1234",
+            afgangsdato="2023-01-01",
+            kladde=False,
+            oprettet_af=self.user,
+        )
+
+        _ = Afgiftsanmeldelse.objects.create(
+            **{
+                "status": "kladde",
+                "leverandørfaktura_nummer": "12345",
+                "betales_af": "afsender",
+                "indførselstilladelse": "abcde",
+                "betalt": False,
+                "fuldmagtshaver": None,
+                "oprettet_af": self.user,
+                "postforsendelse": postforsendelse,
+            }
+        )
+
+        resp = self.client.get(
+            reverse("api-1.0.0:postforsendelse_list"),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+        )
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.json(),
+            {
+                "count": 1,
+                "items": [
+                    {
+                        "id": postforsendelse.id,
+                        "forsendelsestype": Postforsendelse.Forsendelsestype.SKIB[0],
+                        "postforsendelsesnummer": postforsendelse.postforsendelsesnummer,
+                        "afsenderbykode": postforsendelse.afsenderbykode,
+                        "afgangsdato": postforsendelse.afgangsdato.isoformat(),
+                        "kladde": postforsendelse.kladde,
+                    }
+                ],
+            },
+        )
+
+    def test_get_postforsendelse_check_user_permission_denied(self):
+        postforsendelse = Postforsendelse.objects.create(
+            forsendelsestype=Postforsendelse.Forsendelsestype.SKIB,
+            postforsendelsesnummer="1234567890",
+            afsenderbykode="1234",
+            afgangsdato="2023-01-01",
+            kladde=False,
+            oprettet_af=self.user,
+        )
+
+        resp = self.client.get(
+            reverse("api-1.0.0:postforsendelse_get", args=[postforsendelse.id]),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+        )
+
+        self.assertEqual(resp.status_code, 403)
+        self.assertEqual(
+            resp.json(),
+            {
+                "detail": "You do not have permission to perform this action.",
+            },
+        )
