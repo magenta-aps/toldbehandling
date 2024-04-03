9a29d4e8c0e4b20e04ba7a052901a5787c3c3430
Lars Peter Thomsen <larsp@magenta.dk>
2024-04-03 16:34:47 +0200

    fix: [#60322] None-check

--- admin/admin/views.py
+++ admin/admin/views.py
@@ -709,7 +709,9 @@ class AfgiftstabelDetailView(AdminLayoutBaseView, FormView):
 
     def get_initial(self):
         return {
-            "gyldig_fra": self.item.gyldig_fra.strftime(self.form_class.format),
+            "gyldig_fra": self.item.gyldig_fra.strftime(self.form_class.format)
+            if self.item.gyldig_fra
+            else None,
             "kladde": self.item.kladde,
         }
 
