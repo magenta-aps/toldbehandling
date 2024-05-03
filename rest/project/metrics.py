554335fb26e101977719bae5ba81db97ed1e78e1
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-05-03 15:28:30 +0200

    feat(rest): [#60040] added prometheus counter-metric for payments-created and payments-reserved

--- rest/project/metrics.py
+++ rest/project/metrics.py
@@ -2,7 +2,23 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-from prometheus_client import CollectorRegistry, Gauge
+from prometheus_client import CollectorRegistry, Counter, Gauge
+
+# Payment metrics
+
+
+metric_payment_provider_payments_created = Counter(
+    "toldbehandling_payments_created_total",
+    "Total number of provider payments created",
+)
+
+metric_payment_provider_payments_reserved = Counter(
+    "toldbehandling_payments_reserved_total",
+    "Total number of provider payments reserved",
+)
+
+
+# Job metrics
 
 
 def get_job_metric(job_name: str, registry: CollectorRegistry):
