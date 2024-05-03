55f2ccd914a0ee252a72ebd6e98ac0d67011b0e6
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-05-03 10:15:05 +0200

    feat(rest): [#60040] /metrics endpoint now exposes prometheus_client-metrics

--- rest/metrics/api.py
+++ rest/metrics/api.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from ninja_extra import api_controller, permissions, route
 from ninja_jwt.authentication import JWTAuth
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
 
 @api_controller(
@@ -10,4 +12,4 @@ from ninja_jwt.authentication import JWTAuth
 class MetricsAPI:
     @route.get("", auth=JWTAuth(), url_name="metrics_get_all")
     def get_all(self):
-        return "test-asd"
+        return HttpResponse(generate_latest(), content_type=CONTENT_TYPE_LATEST)
