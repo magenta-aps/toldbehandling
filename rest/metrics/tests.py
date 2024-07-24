a105cec5f0fc091eadd9a3cc43ef5bf40b532cff
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-07-24 16:48:13 +0200

    test(rest): [#59939] new tests for metric health check endpoints

--- rest/metrics/tests.py
+++ rest/metrics/tests.py
@@ -2,7 +2,11 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+from unittest.mock import patch
+
 from django.contrib.auth.models import Permission
+from django.test import TestCase
+from django.urls import reverse
 from project.test_mixins import RestMixin
 
 
@@ -24,3 +28,40 @@ class MetricsTest:
         cls.user.user_permissions.add(
             Permission.objects.get(codename="view_privatafgiftsanmeldelse"),
         )
+
+
+class MetricsAPITest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_permissions = []
+        cls.user, cls.user_token, cls.user_refresh_token = RestMixin.make_user(
+            username="metrics-test-user",
+            plaintext_password="testpassword1337",
+            permissions=cls.user_permissions,
+        )
+
+    def test_health_storage(self):
+        resp = self.client.get(
+            reverse("api-1.0.0:metrics_health_storage"),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content.decode(), "OK")
+
+    def test_health_database(self):
+        resp = self.client.get(
+            reverse("api-1.0.0:metrics_health_database"),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content.decode(), "OK")
+
+    @patch("payment.provider_handlers.requests.get")
+    def test_health_payment_providers(self, mock_requests_get):
+        mock_requests_get.return_value.status_code = 405
+        resp = self.client.get(
+            reverse("api-1.0.0:metrics_health_payment_providers"),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_token}",
+        )
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.content.decode(), "OK")
