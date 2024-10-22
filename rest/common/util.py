2b5657f1b2ba0dc667ec79f241f43d0dbda50400
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-10-22 16:48:10 +0200

    feat(rest): [#62382] new common 'send_email'-method

--- rest/common/util.py
+++ rest/common/util.py
@@ -1,5 +1,8 @@
 from datetime import timedelta
+from typing import Any, List, Optional
 
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.utils.datetime_safe import date
 
 
@@ -13,3 +16,31 @@ def dato_næste_måned_start(dato: date) -> date:
 
 def dato_måned_slut(dato: date) -> date:
     return dato_næste_måned_start(dato) - timedelta(days=1)
+
+
+def send_email(
+    subject: str,
+    template: str,
+    context: Optional[dict[str, Any]],
+    from_email: Optional[str],
+    to: Optional[List[str]],
+    bcc: Optional[List[str]],
+    cc: Optional[List[str]],
+    html_template: Optional[str],
+):
+    msg = EmailMultiAlternatives(
+        subject,
+        render_to_string(template, context=context),
+        from_email=from_email,
+        to=to,
+        bcc=bcc,
+        cc=cc,
+    )
+
+    # Configure HTML template if specified
+    if html_template:
+        msg.attach_alternative(
+            render_to_string(html_template, context=context), "text/html"
+        )
+
+    msg.send()
