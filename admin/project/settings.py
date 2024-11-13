6a7421c7558161da2f0d606e8ac6d159d40142d3
Thor Astrup Pedersen <tap@magenta-aps.dk>
2024-11-13 15:26:30 +0100

    feat(admin): [#62382] added EMAIL-settings

--- admin/project/settings.py
+++ admin/project/settings.py
@@ -272,3 +272,19 @@ CSP_SCRIPT_SRC_ATTR = (
 )
 CSP_STYLE_SRC_ATTR = ("'self'",)
 CSP_IMG_SRC = ("'self'", "data:")
+
+# Email configuration
+# Ref: https://docs.djangoproject.com/en/5.0/ref/settings/#email-backend
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = "noreply@nanoq.gl"
+EMAIL_NOTIFICATIONS_ENABLED = bool(
+    strtobool(os.environ.get("EMAIL_NOTIFICATIONS_ENABLED", "False"))
+)
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST", None)
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 25))
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", None)
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", None)
+EMAIL_USE_TLS = bool(strtobool(os.environ.get("EMAIL_USE_TLS", "False")))
+EMAIL_USE_SSL = bool(strtobool(os.environ.get("EMAIL_USE_SSL", "False")))
