2536db225e2ff64fed476b1b9643aaaeec7857c4
Thor Astrup Pedersen <tap@magenta-aps.dk>
2025-07-18 14:08:03 +0200

    test: [#59939] new told_common DateTimeInputTest

--- told-common/told_common/tests/test_form_mixins.py
+++ told-common/told_common/tests/test_form_mixins.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from told_common.form_mixins import FixedWidthIntegerField
+from told_common.form_mixins import DateTimeInput, FixedWidthIntegerField
 
 
 class FixedWidthIntegerFieldTest(TestCase):
@@ -12,3 +14,16 @@ class FixedWidthIntegerFieldTest(TestCase):
 
         self.assertEqual(cm.exception.code, "invalid")
         self.assertIn("Indtast et heltal", str(cm.exception.message))
+
+
+class DateTimeInputTest(TestCase):
+    def test_datetime_input_renders_with_correct_format(self):
+        mixin = DateTimeInput()
+        self.assertEqual(mixin.format, "%Y-%m-%dT%H:%M:%S")
+        self.assertEqual(mixin.input_type, "datetime-local")
+
+    def test_datetime_input_formatting(self):
+        mixin = DateTimeInput()
+        dt = datetime(2025, 7, 18, 15, 30, 45)
+        rendered = mixin.format_value(dt)
+        self.assertEqual(rendered, "2025-07-18T15:30:45")
