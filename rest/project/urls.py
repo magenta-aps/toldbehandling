92402c45f9545ae4ae521936d27f1b05ab144a28
Nick Janssen <nj@magenta-aps.dk>
2025-08-22 11:45:33 +0200

    feat: [#66004] 2FA for django-admin

--- rest/project/urls.py
+++ rest/project/urls.py
@@ -5,12 +5,29 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import redirect
+from django.urls import include, path, reverse
 from django.urls.resolvers import URLPattern, URLResolver
 from project.api import api
+from two_factor.admin import AdminSiteOTPRequired
+from two_factor.urls import urlpatterns as tf_urls
+
+
+class RestAdminSiteOTPRequired(AdminSiteOTPRequired):
+    def login(self, request, **kwargs):
+        if request.user.is_authenticated and not request.user.is_verified():
+            # If the user is logged in but twofactor is not enabled,
+            # Send him to the two-factor setup form
+            return redirect(reverse("two_factor:setup"))
+        else:
+            return super().login(request, **kwargs)
+
+
+admin.site.__class__ = RestAdminSiteOTPRequired
 
 urlpatterns: list[URLResolver | URLPattern] = [
     path("api/admin/", admin.site.urls),
+    path("", include(tf_urls)),
     path("api/", api.urls),
 ]
 
