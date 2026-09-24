06aa435d07a0d802d7d0e1bf9b736108579ebcfe
Lars Peter Thomsen <larsp@magenta.dk>
2026-09-24 11:29:18 +0200

    feat: [#71183] Vis liste af speditører for alle

--- docker/entrypoint_ui.sh
+++ docker/entrypoint_ui.sh
@@ -38,5 +38,7 @@ fi
 
 python manage.py collectstatic --verbosity=0 --no-input
 python manage.py compress --verbosity=1 --force
-
+if [ $ECHO_INTERFACE ]; then
+    echo "Interface: $ECHO_INTERFACE"
+fi
 exec "$@"
